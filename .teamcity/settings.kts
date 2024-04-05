c063eb739583459b39d1c0b8fac02652308d3d9a
thainguyensunya <thainguyensunya@TeamCity>
2024-04-05 10:36:17 +0000

    TeamCity change in 'Hello World Java' project: 'Build' build configuration was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
@@ -31,40 +28,7 @@ version = "2024.03"
 project {
 
     vcsRoot(HttpsGithubComThainguyensunyaHelloWorldJavaRefsHeadsMaster4)
-
-    buildType(Build)
 }
-
-object Build : BuildType({
-    name = "Build"
-
-    vcs {
-        root(HttpsGithubComThainguyensunyaHelloWorldJavaRefsHeadsMaster4)
-    }
-
-    steps {
-        maven {
-            name = "Build and test the app"
-            id = "Build_and_test_the_app"
-            goals = "clean test"
-        }
-        maven {
-            name = "Build and package the app"
-            id = "Build_and_package_the_app"
-            goals = "package"
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
 
 object HttpsGithubComThainguyensunyaHelloWorldJavaRefsHeadsMaster4 : GitVcsRoot({
     name = "https://github.com/thainguyensunya/hello-world-java#refs/heads/master (4)"

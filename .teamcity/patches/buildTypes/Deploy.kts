4ee8ddaf5e68fed8f8a1ce2bdf7f1568ff335c2f
thainguyensunya <thainguyensunya@TeamCity>
2024-04-06 08:33:23 +0000

    TeamCity change in 'Hello World Java' project: artifact dependencies of 'Deploy' build configuration were updated

--- .teamcity/patches/buildTypes/Deploy.kts
+++ .teamcity/patches/buildTypes/Deploy.kts
@@ -43,7 +43,7 @@ create(DslContext.projectId, BuildType({
         }
         artifacts(RelativeId("Deploy")) {
             buildRule = lastSuccessful()
-            artifactRules = "*.jar"
+            artifactRules = "gs-maven-*.jar"
         }
     }
 }))

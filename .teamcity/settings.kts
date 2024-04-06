8591ec82268c56c5ae97e06914858327d354052e
Thai Nguyen <thaiquocnguyen@kms-technology.com>
2024-04-06 16:49:58 +0700

    update pipeline

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
